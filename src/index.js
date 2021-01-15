2bbf4028cbb0dfdbed791fdab3f9b7e73fd30723
jjh204 <59932565+jjh204@users.noreply.github.com>
2021-01-15 15:47:47 +0100

    adding in atatus module to monitor app

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as atatus from 'atatus-spa';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,3 +16,4 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+atatus.config('4102073e1fc743c0b6e44fa01af809f9').install();
