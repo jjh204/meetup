2f081a409b090b270468734ae5f37b28e05f74d1
jjh204 <59932565+jjh204@users.noreply.github.com>
2021-01-19 15:10:56 +0100

    importing the new charts and EventGenre componets

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberEvents from './NumberEvents';
+import EventGenre from './EventGenre';
 import { extractLocations, getEvents } from './api';
 import './nprogress.css';
 import BackGround from './img/background.jpg';
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 class App extends Component {
 
@@ -29,6 +31,16 @@ class App extends Component {
     currentLocation: 'all'
   }
 
+  getData = () => {
+    const { locations, events } = this.state;
+    const data = locations.map((location) => {
+      const number = events.filter((event) => event.location === location).length
+      const city = location.split(' ').shift()
+      return { city, number };
+    })
+    return data;
+  };
+
   updateEvents = (location, eventCount) => {
     getEvents().then((events) => {
       const eventResults = (location === 'all') ?
@@ -46,15 +58,28 @@ class App extends Component {
   render() {
     const { locations, number, events, currentLocation } = this.state
     return (
-      <body style={{ backgroundImage: `url(${BackGround})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', minHeight: '1200px' }}>
+      <div style={{ backgroundImage: `url(${BackGround})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', minHeight: '1200px' }}>
         <div className="App">
           <h1>MEET AND SOCIAL</h1>
-          <h3>events for the social coder</h3>
+          <h3>Events for the Social Coder</h3>
           <CitySearch locations={locations} number={number} updateEvents={this.updateEvents} />
           <NumberEvents currentLocation={currentLocation} number={number} updateEvents={this.updateEvents} />
+          <h3 className="dataTitle">Data display of Events per Genre and City:</h3>
+          <div className="data-vis-wrapper">
+            <EventGenre events={events} />
+            <ResponsiveContainer height={400} >
+              <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20, }}>
+                <CartesianGrid />
+                <XAxis type="category" dataKey="city" name="City" />
+                <YAxis type="number" dataKey="number" name="Number of Events" allowDecimals={false} />
+                <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+                <Scatter data={this.getData()} fill="#1b253a" />
+              </ScatterChart>
+            </ResponsiveContainer>
+          </div>
           <EventList events={events} />
         </div>
-      </body>
+      </div>
     );
   }
 }
